ba113dcd187d9d2a33198551b28114620b575d74
Ashley Hoath <ahoath87@gmail.com>
2023-04-10 13:12:48 -0600

    I got it to work! The radio buttons see merge comments for notes

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -2,36 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { getAllTheSymptoms } from '../api/symptoms';
 
 const Quiz = () => {
-  const [symptomChoice, setSymptomChoice] = useState(0);
+  const [symptomChoice, setSymptomChoice] = useState(null);
   const [symptoms, setSymptoms] = useState([]);
-  const [selected, setSelected] = useState(false);
+  // const [selected, setSelected] = useState(false);
 
   // console.log('symptoms in QUIZ', symptoms);
 
   useEffect(() => {
     const allSymps = async () => {
       const fetchedSymps = await getAllTheSymptoms();
-      // console.log('this is fecthedSymps', fetchedSymps);
+      console.log('this is fecthedSymps', fetchedSymps);
       setSymptoms(fetchedSymps);
     };
     allSymps();
   }, []);
 
-  // const submitHandler = async (e) => {
-  //   try {
-  //     e.prevenDefault();
-  //     console.log('gotcha symptom buddy');
-
-  //     console.log('BOOM BABY', symptomChoice);
-  //   } catch (error) {
-  //     console.errer(error);
-  //   }
-  // };
+  const submitHandler = async (e) => {
+    try {
+      if (symptomChoice == null) {
+        alert('You must make a selection before submitting');
+      } else {
+        console.log('BOOM BABY', symptomChoice);
+        console.log('this is symptomChoice', symptomChoice);
+        e.preventDefault();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   function onChangeValue(event) {
     setSymptomChoice(event.target.value);
-    setSelected(true);
-    console.log('this is symptomChoice', symptomChoice);
+    // setSelected(true);
+    // console.log('this is selected', selected);
   }
   return (
     <div>
@@ -48,7 +51,7 @@ const Quiz = () => {
                       value={symptom.id}
                       name={'firstsymptom'}
                       onChange={onChangeValue}
-                      defaultChecked={selected}
+                      // defaultChecked={false}
                     ></input>
                     {symptom.description}
                   </label>
@@ -56,11 +59,11 @@ const Quiz = () => {
               );
             })}
         </div>
-        {/* <div>
-          <button className='btn-quiz1' type='submit' onClick={submitHandler}>
+        <div>
+          <button className='btn-quiz1' type='button' onClick={submitHandler}>
             Submit
           </button>
-        </div> */}
+        </div>
       </form>
     </div>
   );
