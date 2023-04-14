fa4123654b91d26a04b74ad555f42bb0441fe141
Ashley Hoath <ahoath87@gmail.com>
2023-04-14 14:05:19 -0600

    continue to style

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/info.css';
 
 const Info = () => {
   return (
     <div className='info-container'>
-      This is Info
       <div className='quizboxlink'>
-        <div>Take a symptom quiz to help identifiy whats going</div>
         <div>
-          <p>Click Here!</p>
+          Take our symptom quiz to gain access to suggested treatments and
+          diagnosis
+        </div>
+        <div>
+          <Link className='clickhere'>
+            <p id='clickhere'>Click Here!</p>
+          </Link>
         </div>
       </div>
-      <div>Not Sure if this is speaking to ya.... keep scrolling</div>
-      <div>
+      <div className='facts'>
+        Not Sure if this is speaking to ya.... keep scrolling
+      </div>
+      <div className='factlist'>
         <div>FACT 1</div>
         <div>FACT 2</div>
         <div>FACT 3</div>
